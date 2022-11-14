1cf4edad750f746ef6da7cb470bbb00cc089970e
Vlado Cavric <vladocavric@gmail.com>
2022-11-15 00:12:09 +0100

    nav almost finished

--- src/components/Layout/MainNav.jsx
+++ src/components/Layout/MainNav.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
 
 import styles from './MainNav.module.scss'
 
 const MainNavigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenuHandler = () => {
+        setIsOpen(prevState => !prevState)
+    }
    const  mouseOverHandle = () => {}
   return (
     <nav className={styles.MainNav}>
@@ -13,10 +17,10 @@ const MainNavigation = () => {
         </NavLink>
 
     </div>
-    <ul className="c-nav-ul c-txt-trans-c">
-        <li><NavLink to="#">home</NavLink></li>
-        <li><NavLink to="#">about</NavLink></li>
-        <li className={styles.MainNav__Dropdown}>
+    <ul className={`${isOpen ? styles.MainNav__IsOpen : ''}`}>
+        <li style={{'--time': '0.2s'}}><NavLink to="#" >home</NavLink></li>
+        <li style={{'--time': '0.4s'}}><NavLink to="#" >about</NavLink></li>
+        <li className={styles.MainNav__Dropdown} style={{'--time': '0.6s'}}>
             <NavLink to="#" onMouseOver={mouseOverHandle}>work</NavLink>
             <div className={styles.MainNav__Dropdown__Content} id="d-none-tablet">
                     <NavLink to="#">Link 1 Link 1 Link 1</NavLink>
@@ -24,14 +28,14 @@ const MainNavigation = () => {
                     <NavLink to="#">Link 3</NavLink> 
                 </div>
         </li>
-        <li><NavLink to="#">projects</NavLink></li>
+        <li style={{'--time': '0.8s'}}><NavLink to="#">projects</NavLink></li>
     </ul>
   
-    <div className="c-nav-burger burger">
-        <div className="c-nav-burger-l1 u-bg-w"></div>
-        <div className="c-nav-burger-l2 u-bg-w"></div>
-        <div className="c-nav-burger-l3 u-bg-w"></div>
-    </div>
+    <button onClick={toggleMenuHandler} className={`${styles.MainNav__Hamburger} ${isOpen ? styles.MainNav__IsOpen : ''}`}>
+        <span className="c-nav-burger-l1"></span>
+        <span className="c-nav-burger-l2"></span>
+        <span className="c-nav-burger-l3"></span>
+    </button>
 </nav>
   )
 }
